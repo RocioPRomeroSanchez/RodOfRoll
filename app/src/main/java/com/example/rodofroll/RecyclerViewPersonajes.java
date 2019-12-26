bcaa4b88f6f0af91063609621adefc3623d85076
Chio98 <rorocor98@gmail.com>
2019-12-26 13:04:08 +0100

    haciendo un commit local

---
+++ app/src/main/java/com/example/rodofroll/RecyclerViewPersonajes.java
@@ -0,0 +1,60 @@
+package com.example.rodofroll;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+
+public class RecyclerViewPersonajes extends Fragment {
+
+    Adapter adapter= null;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.recyclerpersonajes, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recycler);
+
+        try {
+            adapter = new Adapter(getContext());
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false));
+
+        return view;
+    }
+
+
+
+}
